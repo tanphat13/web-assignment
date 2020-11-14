e801b6d1c713dd4f20c5cb0cc20a32982453a5b8
AnhTuan4198 <whitejunh@gmail.com>
2020-11-14 10:41:33 +0700

    Done handle login form

--- public/js/login-form.js
+++ public/js/login-form.js
@@ -2,11 +2,10 @@ document.addEventListener('DOMContentLoaded',function(){
     let loginForm = document.getElementById("loginForm");
     let loginBtn = document.getElementById("loginBtn")
     //loginForm.classList.toggle('active');
-    loginBtn.onclick = function(){
-         console.log(loginBtn); 
-         loginForm.classList.toggle("active");
-    } 
-
-    
-
+   if(loginBtn){
+        loginBtn.onclick = function () {
+          console.log(loginBtn);
+          loginForm.classList.toggle("active");
+        }; 
+   }
 },false);
