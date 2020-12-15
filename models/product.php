ad7ac9425d3e5587a5c01a203c50ef7a76e621db
tanphat13 <phat.cu_bkhcm1312@hcmut.edu.vn>
2020-12-15 14:02:32 +0700

    Updating

--- models/product.php
+++ models/product.php
@@ -119,5 +119,32 @@ class Product extends DbModel {
             $sql_command->execute();
             return $sql_command->fetchAll(PDO::FETCH_ASSOC);
         }
+
+        public function searchProducts($key) {
+            $sql_command = self::prepare("SELECT products.product_id, products.product_name, products.product_price, products.product_color, products.product_ram, products.product_rom, products.rating, MIN(images.image_id), images.link 
+            FROM products LEFT JOIN images ON products.product_id = images.product_id WHERE (products.product_name LIKE '%$key%' OR products.product_brand LIKE '%$key%') GROUP BY products.product_name ORDER BY products.rating;");
+            $sql_command->execute();
+            $products_list = $sql_command->fetchAll(PDO::FETCH_ASSOC);
+            $products_element = "";
+            foreach ($products_list as $product) {
+            $formatted_price = number_format($product['product_price'], 0, '', '.');
+            $products_element .= "
+                <div class='product-item' id='product-item-$product[product_id]'>
+                    <div class='product-image'>
+                        <div class='img'><img src='$product[link]' alt='$product[product_name]' /></div>
+                    </div>
+                    <div class='product-info'>
+                        <div class='product-spec'>
+                            <p class='info name' id='product-$product[product_id]'>$product[product_name] ($product[product_ram] GB/$product[product_rom] GB)</p>
+                            <p class='info'>Color: $product[product_color]</p>
+                        </div>
+                        <span id='price' class='price'> $formatted_price VND</span>
+                    </div>
+                    <button type='button' class='delete-btn' onclick='confirmRemoveProduct($product[product_id])'>Add</button>
+                </div>
+            ";
+        }
+        return $products_element;
+        }
     }
 ?>
