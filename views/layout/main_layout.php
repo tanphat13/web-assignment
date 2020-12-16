ec886b9148fbfe7d746b283057d2d783ab281cb5
tanphat13 <phat.cu_bkhcm1312@hcmut.edu.vn>
2020-12-16 08:34:32 +0700

    Semi-final

--- views/layout/main_layout.php
+++ views/layout/main_layout.php
@@ -73,7 +73,7 @@ use app\core\Application;
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
